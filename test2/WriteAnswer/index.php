5ad1fa3383b3830a49bdac0e962096ff340d58b8
STeLSuK <666neke666@gmail.com>
2019-12-10 00:30:06 +0400

    test2

---
+++ test2/WriteAnswer/index.php
@@ -0,0 +1,9 @@
+<?php 
+  $a = $_POST['input'];
+  $primerString = '{"SumResult":30,"MulResult":4,"SortedInputs":["1.00","1.01","2.02","4.00"]}';
+  if ($a == $primerString) {
+  	echo "Тест пройден";
+  } else {
+  	echo "Тест не пройден";
+  }
+ ?>
